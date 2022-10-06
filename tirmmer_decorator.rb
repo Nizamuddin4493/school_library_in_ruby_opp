0657531b088805d4320bb8e30b632d0655e31062
Nizamuddin Ahmadzai <78977308+Nizamuddin4493@users.noreply.github.com>
2022-10-06 21:43:56 +0430

    TrimmerDecorator inherit from BaseDecoratore class

--- tirmmer_decorator.rb
+++ tirmmer_decorator.rb
@@ -1,6 +1,6 @@
 require_relative 'base_decorator'
 
-class TrimmerDecorator
+class TrimmerDecorator < BaseDecorator
   def correct_name
     if @nameable.correct_name.length > 10
       @nameable.correct_name[0, 10]
