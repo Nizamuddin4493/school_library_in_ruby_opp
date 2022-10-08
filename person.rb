1ae364df102e9871ab8d0054709f4ed26d40e9d8
Nizamuddin Ahmadzai <78977308+Nizamuddin4493@users.noreply.github.com>
2022-10-09 01:46:22 +0430

    adding and removing some variable

--- person.rb
+++ person.rb
@@ -2,13 +2,13 @@ require_relative 'nameable'
 
 class Person < Nameable
   attr_accessor :name, :age, :parent_permission
-  attr_reader :id
+  attr_reader :id, :rentals
 
   def initialize(age, name = 'unknown', parent_permission: true)
     super()
     @id = Random.rand(1..1000)
-    @name = name
     @age = age
+    @name = name
     @parent_permission = parent_permission
     @rentals = []
   end
