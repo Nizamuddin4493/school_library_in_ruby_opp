9e20a7f9b09197deb982fe18eb2c162d7ea3c8d6
Nizamuddin Ahmadzai <78977308+Nizamuddin4493@users.noreply.github.com>
2022-10-14 13:23:47 +0430

    class Book test completed

--- spec/book_rspec.rb
+++ spec/book_rspec.rb
@@ -1,12 +1,12 @@
 require_relative './spec_helper'
 
 describe Book do
-  it 'creates a new instance of the BaseDecorator class' do
+  it 'creates a new instance of the Book class' do
     book = Book.new('title', 'auther')
     expect(book).to be_an_instance_of Book
   end
 
-  it 'adds a student object to the array' do
+  it 'adds a rentals object to the array' do
     rental = {}
     name = Book.new('Js Master', 'John')
     name.add_rental(rental)
